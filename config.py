17e3d782881dff4906d647581e70fe9838e7317b
ultron-probot <smitjhn392@gmail.com>
2025-12-29 13:11:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 # ===============================================
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/PURVI_BOTS")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/PURVI_UPDATES")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+EhhBpbVJHkM0NDg9")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/A2globalupdate")
 
 # ===============================================
 
